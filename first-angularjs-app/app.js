e702b406c4f030843b4c546e3370afb3e8b79e38
Guilherme Mello <guilhermemello@Guilhermes-MacBook-Pro.local>
2015-02-19 12:43:31 +0000

    First commit

---
+++ first-angularjs-app/app.js
@@ -0,0 +1,17 @@
+(function(){
+  var myApp = angular.module('myApp', []);
+
+  // Controller
+  myApp.controller("myController", function(){
+
+    this.user = obj;
+
+  });
+
+  var obj = {
+    name: 'Guilherme',
+    password: '123',
+    age: '23'
+  }
+
+})();
